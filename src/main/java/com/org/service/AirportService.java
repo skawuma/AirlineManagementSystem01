e11d4c67ec21e5db9b9cd8b8cceef4a993ae1212
skawuma <82976186+skawuma@users.noreply.github.com>
2023-01-11 08:18:38 -0500

    started on service layer... still in progress

---
+++ src/main/java/com/org/service/AirportService.java
@@ -0,0 +1,19 @@
+package com.org.service;
+
+import org.springframework.http.ResponseEntity;
+
+import com.org.entity.Airport;
+
+public interface AirportService {
+	
+	public Iterable <Airport> viewAllAirport();
+	
+	public Airport viewAirport(String airportCode);
+	
+	public ResponseEntity<?> addAirport(Airport airport);
+	
+	public Airport modifyAirport(Airport airport);
+	
+	public String removeAirport(String airportCode);
+
+}
