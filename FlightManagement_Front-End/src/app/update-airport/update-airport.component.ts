ec6eaa9c80fd5cdac78a7d458b76cf072b8a61b0
skawuma <82976186+skawuma@users.noreply.github.com>
2023-03-25 14:18:15 -0400

    update Airport Completed

--- FlightManagement_Front-End/src/app/update-airport/update-airport.component.ts
+++ FlightManagement_Front-End/src/app/update-airport/update-airport.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Airport } from '../airport';
+import { AirportService } from '../airport.service';
 
 @Component({
   selector: 'app-update-airport',
@@ -7,9 +10,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UpdateAirportComponent implements OnInit {
 
-  constructor() { }
+  airportCode:any;
+  airport:Airport | any;
+  constructor(private route: ActivatedRoute,private router: Router,
+    private airportService: AirportService) { }
 
-  ngOnInit(): void {
+  ngOnInit(){
+    this.airport = new Airport();
+
+    this.airportCode= this.route.snapshot.params['airportCode'];
+
+    this.airportService.viewAirport(this.airportCode)
+      .subscribe(data => {
+        console.log(data)
+        this.airport = data;
+      }, error => console.log(error));
+  }
+  updateAirport() {
+    this.airportService.modifyAirport(this.airportCode, this.airport)
+      .subscribe(data => console.log(data), error => console.log(error));
+    this.airport = new Airport();
+    this.gotoList();
+  }
+
+  onSubmit() {
+    this.updateAirport();
+  }
+
+  gotoList() {
+    this.router.navigate(['/airports']);
   }
 
 }
